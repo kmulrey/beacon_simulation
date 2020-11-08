0aca8b392f01df5757e88353bcc40b512c8c4be4
Katie <kmurley@gmail.com>
2020-11-08 20:47:40 +0100

    .

--- geant/make_jobs.py
+++ geant/make_jobs.py
@@ -12,7 +12,7 @@ def write_file(energy, zenith, azimuth):
 
     filename=run_dir+'run_zhaires_'+str(energy)+'_'+str(zenith)+'_'+str(azimuth)+'.q'
     print filename
-    '''
+    
     outfile=open(filename,'w')
 
     outfile.write('#! /bin/bash\n')
@@ -23,7 +23,7 @@ def write_file(energy, zenith, azimuth):
     outfile.write('. /vol/optcoma/geant4_9.6_install/share/Geant4-9.6.4/geant4make/geant4make.sh\n')
     outfile.write('cd {0}/beacon_simulation/geant/\n'.format(base_dir))
     outfile.write('./BEACON_simulation {0} {1} {2}\n'.format(str(energy),str(zenith), str(azimuth)))
-    '''
+
     
     
 energy=[16.5,17.0,17.5,18.0,18.5,19.0]
