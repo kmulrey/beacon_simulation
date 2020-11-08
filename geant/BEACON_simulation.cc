c6e0fb6163a8c178663ba7ccde6e5ad3c04a9a55
Katie <kmurley@gmail.com>
2020-11-08 21:34:42 +0100

    .

--- geant/BEACON_simulation.cc
+++ geant/BEACON_simulation.cc
@@ -228,10 +228,7 @@ int main(int argc,char** argv)
             // or energy density in MeV / m^2 ? It seems MeV / m^2, I would say, as then
             // the area correction factor is right.
             // The comparison is with LORA MeV in detector, however...
-             //cout<<i << "    "<<DepositedEnergyElecPosi[i]<<"  "<< //2.25*cos(prim_zenith)*DepositedEnergyTotal[i]/TotalArea[i] << "    " << //int(100.*DepositedEnergyGamma[i]/DepositedEnergyTotal[i])
-                 //          << "   " << int(100.*DepositedEnergyElecPosi[i]/DepositedEnergyTotal[i])
-                  //         << "   " << int(100.*DepositedEnergyMuons[i]/DepositedEnergyTotal[i])
-                 //       << "   " << int(100.*DepositedEnergyHadrons[i]/DepositedEnergyTotal[i]) << endl;
+             
             myfile << i << "     " << 2.25*cos(prim_zenith)*DepositedEnergyTotal[i]/TotalArea[i] << "    " << int(100.*DepositedEnergyGamma[i]/DepositedEnergyTotal[i])
                << "   " << int(100.*DepositedEnergyElecPosi[i]/DepositedEnergyTotal[i]) 
                << "   " << int(100.*DepositedEnergyMuons[i]/DepositedEnergyTotal[i])
